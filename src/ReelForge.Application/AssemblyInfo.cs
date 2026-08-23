ab30276f70a0aa8a0bc4e36a08de5c6fc2665819
Mathew Levasseur <5962839+matlev@users.noreply.github.com>
2026-08-23 11:36:45 -0700

    Split portable tests by layer

--- src/ReelForge.Application/AssemblyInfo.cs
+++ src/ReelForge.Application/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ReelForge.Tests")]
+[assembly: InternalsVisibleTo("ReelForge.Application.Tests")]
 [assembly: InternalsVisibleTo("ReelForge.App")]
